be4c81b27cfb3d5466d10901b64101c17692484d
osparking <jbpark03@gmail.com>
2023-08-13 13:01:50 +0900

    custom once per request filter

---
+++ src/main/java/space/bum/config/security/filters/CustomAuthFilter.java
@@ -0,0 +1,24 @@
+package space.bum.config.security.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class CustomAuthFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(
+			HttpServletRequest request,
+			HttpServletResponse response, 
+			FilterChain filterChain) throws ServletException, IOException {
+		
+		filterChain.doFilter(request, response); // 인증 작동 때만
+	}
+}

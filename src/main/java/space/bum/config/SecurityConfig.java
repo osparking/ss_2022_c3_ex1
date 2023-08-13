f1271c35ca0a4e6203d9c7da09482a0a303a7189
osparking <jbpark03@gmail.com>
2023-08-13 13:22:54 +0900

    add custom filter into filter chain

--- src/main/java/space/bum/config/SecurityConfig.java
+++ src/main/java/space/bum/config/SecurityConfig.java
@@ -1,8 +1,26 @@
 package space.bum.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.AllArgsConstructor;
+import space.bum.config.security.filters.CustomAuthFilter;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
 
+	private final CustomAuthFilter customAuthFilter;
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.addFilterAt(customAuthFilter,
+				UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests().anyRequest().authenticated() // learn later
+				.and()
+				.build();
+	}
 }

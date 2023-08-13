e93e8bf8cdf81a85be1f223d532de04598372cc4
osparking <jbpark03@gmail.com>
2023-08-13 23:48:36 +0900

    manager uses provider and authenticates

--- src/main/java/space/bum/config/security/managers/CustomAuthenManager.java
+++ src/main/java/space/bum/config/security/managers/CustomAuthenManager.java
@@ -1,17 +1,28 @@
 package space.bum.config.security.managers;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+
 @Component
+@AllArgsConstructor
 public class CustomAuthenManager implements AuthenticationManager {
 
+	private AuthenticationProvider provider;
+	
 	@Override
 	public Authentication authenticate(Authentication authentication)
 			throws AuthenticationException {
-		return null;
+		
+		if (provider.supports(authentication.getClass())) {
+			return provider.authenticate(authentication);
+		}
+		throw new BadCredentialsException("인증 공급자 부재");
 	}
 
 }

33776177f5e88f64b1bc1feb3165c187aa526f09
osparking <jbpark03@gmail.com>
2023-08-13 23:37:06 +0900

    key member init value is key property value

--- src/main/java/space/bum/config/security/providers/CustomAuthenProvider.java
+++ src/main/java/space/bum/config/security/providers/CustomAuthenProvider.java
@@ -1,5 +1,6 @@
 package space.bum.config.security.providers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -10,10 +11,13 @@ import space.bum.config.security.authen.CustomAuthen;
 @Component
 public class CustomAuthenProvider implements AuthenticationProvider {
 
+	@Value("${our.very.secret.key}")
+	private String key;
+	
 	@Override
 	public Authentication authenticate(Authentication authentication)
 			throws AuthenticationException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 

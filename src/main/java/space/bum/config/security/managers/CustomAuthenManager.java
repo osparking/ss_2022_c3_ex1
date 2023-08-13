4fb443561484c492c7ebfb5581a1e0c34bf296f3
osparking <jbpark03@gmail.com>
2023-08-13 15:28:10 +0900

    Authen manager implements Authen Manager

--- src/main/java/space/bum/config/security/managers/CustomAuthenManager.java
+++ src/main/java/space/bum/config/security/managers/CustomAuthenManager.java
@@ -1,8 +1,17 @@
 package space.bum.config.security.managers;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomAuthenManager {
+public class CustomAuthenManager implements AuthenticationManager {
+
+	@Override
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException {
+		return null;
+	}
 
 }
